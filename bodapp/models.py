c5ff261e7bf6782a109ef5c6ac6a49ad6e2919bd
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-11-17 16:59:44 +0000

    create basic booking model and migrate changes to database

--- bodapp/models.py
+++ bodapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Name(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.user
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.CharField(max_length=200)
+    time = models.TimeField()
+
+    def __str__(self):
+        return str(self.user)
