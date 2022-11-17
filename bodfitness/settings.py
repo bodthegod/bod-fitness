bfb7677d20aa8b8b47625bf5aed65c40d5142758
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-11-17 17:18:06 +0000

    fix bug with locally hosting server

--- bodfitness/settings.py
+++ bodfitness/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bodfitness.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['bodfitness.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
