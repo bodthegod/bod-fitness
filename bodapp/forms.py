2f085cfa5a1c95057efc04a3c47bcd6ab534c129
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-11-22 14:58:43 +0000

    edit dateform class for date functionality

--- bodapp/forms.py
+++ bodapp/forms.py
@@ -3,8 +3,5 @@ from django.contrib.admin import widgets
 from .models import Booking
 
 
-class BookingForm(forms.ModelForm):
-    class Meta:
-        model = Booking
-
-        fields = ['name', 'date']
+class DateForm(forms.Form):
+    date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
