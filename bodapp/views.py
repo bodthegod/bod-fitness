c38e29155acd333c8c769ddcc0ee26ed50160098
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-11-22 15:48:04 +0000

    create basic views.py for homeview and formview

--- bodapp/views.py
+++ bodapp/views.py
@@ -1,3 +1,14 @@
+from django.views.generic import TemplateView, FormView
 from django.shortcuts import render
 from django.views import generic
+from .forms import DateForm
 from .models import Booking, Name
+
+
+class HomeView(TemplateView):
+    template_name = 'bodfitness/base.html'
+
+
+class FormView(FormView):
+    form_class = DateForm
+    template_name = 'bodfitness/index.html'
