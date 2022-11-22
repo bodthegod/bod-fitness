edd8ef1d34d1f9875686ca222afe0b333ba1fd18
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-11-22 16:26:34 +0000

    fix views not showing

--- bodapp/views.py
+++ bodapp/views.py
@@ -2,13 +2,13 @@ from django.views.generic import TemplateView, FormView
 from django.shortcuts import render
 from django.views import generic
 from .forms import DateForm
-from .models import Booking, Name
+# from .models import Booking, Name
 
 
 class HomeView(TemplateView):
-    template_name = 'bodfitness/base.html'
+    template_name = 'base.html'
 
 
 class FormView(FormView):
     form_class = DateForm
-    template_name = 'bodfitness/index.html'
+    template_name = 'index.html'
