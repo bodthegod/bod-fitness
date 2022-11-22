22d3f02bc11192a5b8eae3ed73eb8cc5e7de5aad
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-11-22 15:35:14 +0000

    update forms.py functionality with fields

--- bodapp/forms.py
+++ bodapp/forms.py
@@ -3,5 +3,9 @@ from django.contrib.admin import widgets
 from .models import Booking
 
 
-class DateForm(forms.Form):
+class DateForm(forms.ModelForm):
     date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
+
+    class Meta:
+        model = Booking
+        fields = ('name', 'date')
