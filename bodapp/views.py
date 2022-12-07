2dbb8dd14e8ee2bbd8f4d27153705eab4e48d7a3
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-12-07 16:22:28 +0000

    define booking function to post choices to db

--- bodapp/views.py
+++ bodapp/views.py
@@ -1,8 +1,24 @@
 from django.views.generic import TemplateView, FormView
-from django.shortcuts import render
+from datetime import datetime, timezone
+from django.shortcuts import render, redirect
 from django.views import generic
+from django.contrib import messages
+from .models import Booking
 from .forms import DateForm
-# from .models import Booking, Name
+
+def booking(request):
+    #Calls allowedday function to show next two weeks of days
+    days = allowedDay(15)
+
+    #Allows user to see unbooked days
+    availableDays = dayValidCheck(days)
+
+
+    if request.method == 'POST':
+        advice = request.POST.get('advice')
+        choice = request.POST.get('choice')
+        date = request.POST.get('date')
+
 
 
 class HomeView(TemplateView):
