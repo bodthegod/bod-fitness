4ffed4dfb3f5bc356189feaaa8191e88d99b8c01
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-12-07 15:10:49 +0000

    add user choices to database model

--- bodapp/models.py
+++ bodapp/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from datetime import datetime
 from django.contrib.auth.models import User
 from django.utils import timezone
+
+ADVICE_TOPIC = (
+    ("Weight Gain", "Weight Gain"),
+    ("Weight Loss", "Weight Loss"),
+    ("Endurance", "Endurance"),
+    ("Strength Gain", "Strength Gain"),
+)
+
+BOOKING_CHOICE = (
+    ("Online", "Online"),
+    ("In Person", "In Person"),
+)
+
+
 
 
 class Name(models.Model):
@@ -12,9 +27,9 @@ class Name(models.Model):
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    advice = models.CharField(max_length=15, choices=ADVICE_TOPIC, default="Weight Gain")
+    choice = models.CharField(max_length=15, choices=BOOKING_CHOICE, default="Online")
     date = models.DateField(default=timezone.now)
-    time = models.TimeField()
-    name = models.CharField(max_length=30)
 
     def __str__(self):
-        return str(self.user)
+        return f"{self.user.username} | date {self.date}"
