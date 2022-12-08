c69374af4923d08d18045aecdbc6a4efecedb0dc
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-12-08 16:12:05 +0000

    add finalisebooking function

--- bodapp/views.py
+++ bodapp/views.py
@@ -1,13 +1,15 @@
-from django.views.generic import TemplateView, FormView
-from datetime import datetime, timezone
 from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, FormView
+from datetime import datetime, timezone, timedelta
 from django.views import generic
 from django.contrib import messages
-from .models import Booking
+from .models import *
 from .forms import DateForm
 
+
 def index(request):
-    return render(request, "index.html",{})
+    return render(request, "index.html", {})
+
 
 def booking(request):
     daysfor = daysFor(15)
@@ -17,20 +19,36 @@ def booking(request):
     if request.method == 'POST':
         advice = request.POST.get('advice')
         choice = request.POST.get('choice')
+        date = request.POST.get('date')
         if advice == None:
             messages.success(request, "Select some Advice")
-            return redirect('booking')
+            return redirect('booking_form')
 
         if choice == None:
             messages.success(request, "Select a Choice")
-            return redirect('booking')
+            return redirect('booking_form')
 
-    # Stored user choices in django
-    request.session['advice'] = advice
-    request.session['choice'] = choice
-    request.session['date'] = date
+        # Stored user choices in django
+        request.session['advice'] = advice
+        request.session['choice'] = choice
+        request.session['date'] = date
 
-    return redirect('finaliseBooking')
+        return redirect('finaliseBooking')
+
+    return render(request, 'index.html', {
+            'daysfor': daysfor,
+            'availabledays': availableDays,
+        })
 
 
+def finaliseBooking(request):
+    today = datetime.now()
+    user = request.user
+    preDate = today.strftime('%Y-%m-%d')
+    deltatime = today + timedelta(days=15)
+    strdeltatime = deltatime.strftime('%Y-%m-%d')
+    topDate = strdeltatime
 
+    advice = request.session.get('advice')
+    choice = request.session.get('choice')
+    date = request.session.get('day')
