33da152a0af21f155dd18f788c238d45f373592f
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-12-15 17:38:49 +0000

    remove old selection functionality to bugfix model

--- bodapp/models.py
+++ bodapp/models.py
@@ -3,23 +3,21 @@ from datetime import datetime
 from django.contrib.auth.models import User
 
 
-ADVICE_TOPIC = (
-    ("Weight Gain", "Weight Gain"),
-    ("Weight Loss", "Weight Loss"),
-    ("Endurance", "Endurance"),
-    ("Strength Gain", "Strength Gain"),
-)
+# ADVICE_TOPIC = (
+#     ("Weight Gain", "Weight Gain"),
+#     ("Weight Loss", "Weight Loss"),
+#     ("Endurance", "Endurance"),
+#     ("Strength Gain", "Strength Gain"),
+# )
 
-BOOKING_CHOICE = (
-    ("Online", "Online"),
-    ("In Person", "In Person"),
-)
+# BOOKING_CHOICE = (
+#     ("Online", "Online"),
+#     ("In Person", "In Person"),
+# )
 
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    advice = models.CharField(max_length=15, choices=ADVICE_TOPIC, default="Weight Gain")
-    choice = models.CharField(max_length=15, choices=BOOKING_CHOICE, default="Online")
     date = models.DateField(default=datetime.now)
 
     def __str__(self):
