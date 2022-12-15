18438981719debaeca2c0a20b8745101578015ca
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-12-15 17:35:01 +0000

    create bookingadmin class to hold fields in admin panel

--- bodapp/admin.py
+++ bodapp/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Booking
+from .models import *
 
-admin.site.register(Booking)
+
+class BookingAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('booking_number',)
+
+    fields = ('user', 'advice', 'choice', 'date', 'booking_number')
+
+    list_display = ('user', 'advice', 'choice', 'date', 'booking_number')
+
+    ordering = ('-date',)
+
+
+admin.site.register(Booking, BookingAdmin)
