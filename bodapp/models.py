6dc75fabd466b5944cb18eb7f18cd5b163033d5e
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-12-15 17:41:38 +0000

    rework choice selection for models

--- bodapp/models.py
+++ bodapp/models.py
@@ -20,6 +20,32 @@ class Booking(models.Model):
     booking_number = models.CharField(
         max_length=32, null=False, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    WEIGHT_GAIN = 1
+    WEIGHT_LOSS = 2
+    ENDURANCE = 3
+    STRENGTH_GAIN = 4
+
+    ADVICE_CHOICE = (
+        (WEIGHT_GAIN, ('Weight Gain')),
+        (WEIGHT_LOSS, ('Weight Loss')),
+        (ENDURANCE, ('Endurance')),
+        (STRENGTH_GAIN, ('Strength Gain')),)
+    advice = models.PositiveSmallIntegerField(
+        choices=ADVICE_CHOICE,
+        default=WEIGHT_GAIN,
+    )
+
+    ONLINE = 1
+    IN_PERSON = 2
+
+    BOOKING_CHOICE = (
+        (ONLINE, ('Online')),
+        (IN_PERSON, ('In Person')),
+    )
+    choice = models.PositiveSmallIntegerField(
+        choices=BOOKING_CHOICE,
+        default=ONLINE,
+    )
     date = models.DateField(default=datetime.now)
 
     def _generate_booking_number(self):
@@ -31,5 +57,4 @@ class Booking(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.user.username} | date {self.date}"
         return self.booking_number
