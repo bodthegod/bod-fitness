a605c2d461fdd5d83f47711bb58f8096d235c49f
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-12-15 17:41:05 +0000

    create bookingnumber generation per booking

--- bodapp/models.py
+++ bodapp/models.py
@@ -17,8 +17,19 @@ from django.contrib.auth.models import User
 
 
 class Booking(models.Model):
+    booking_number = models.CharField(
+        max_length=32, null=False, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(default=datetime.now)
 
+    def _generate_booking_number(self):
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        if not self.booking_number:
+            self.booking_number = self._generate_booking_number()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.user.username} | date {self.date}"
+        return self.booking_number
