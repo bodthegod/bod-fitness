0704375bdae67f9b7f3165ee86e432712bdb6634
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-12-21 17:29:02 +0000

    add admin interface

--- bodfitness/settings.py
+++ bodfitness/settings.py
@@ -39,6 +39,8 @@ ALLOWED_HOSTS = ['bodfitness.herokuapp.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
