fe37bd9c979e69164578749b30e9dc61993e7839
bodthegod <77025894+bodthegod@users.noreply.github.com>
2022-12-28 23:27:47 +0000

    fix bug with site not displaying

--- bodfitness/settings.py
+++ bodfitness/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEVELOPMENT')
 
 ALLOWED_HOSTS = ['bodfitness.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'bodapp',
 ]
 
-SITE_ID = 1
+SITE_ID = 2
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
